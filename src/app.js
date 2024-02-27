535575da5d0af50aee35ddf0815a636ff9e804a7
Cristales <mariocardona0501@gmail.com>
2024-02-27 01:14:31 -0600

    Notas Controller

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import estudiantesRoutes from "./routes/estudiantes.routes.js"
 import cursosRoutes from "./routes/cursos.routes.js"
 import personalRoutes from "./routes/personal.routes.js"
 import asistenciasRoutes from "./routes/asistencias.routes.js"
+import notasRoutes from "./routes/notas.routes.js"
 
 const app = express()
 
@@ -20,5 +21,6 @@ app.use('/api/estudiantes', estudiantesRoutes)
 app.use('/api/cursos', cursosRoutes)
 app.use('/api/personal', personalRoutes)
 app.use('/api/asistencias', asistenciasRoutes)
+app.use('/api/notas', notasRoutes)
 
 export default app
