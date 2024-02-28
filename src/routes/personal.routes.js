61c8072f2a9af1fc906611bfd1ecfbf454a7aa7c
Cristales <mariocardona0501@gmail.com>
2024-02-27 20:45:33 -0600

    Despedir Personal

--- src/routes/personal.routes.js
+++ src/routes/personal.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express"
 
 import personalController from "../controllers/personal.controllers.js"
-const { getPersonal, postPersonal } = personalController
+const { getPersonal, postPersonal, deletePersonal, getPersonalByCode } = personalController
 
 const router = Router()
 
 router.route("/")
     .get(getPersonal)
     .post(postPersonal)
+
+router.route("/:codigo_personal")
+    .get(getPersonalByCode)
+    .delete(deletePersonal)
 
 export default router
